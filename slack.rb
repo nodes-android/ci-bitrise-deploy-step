f231e8eeea1b7f69d077ca8da5895949a644c2b7
Johnny Sørensen <post@johsoe.dk>
2018-04-24 13:58:02 +0200

    gave the slack output some love

--- slack.rb
+++ slack.rb
@@ -4,7 +4,7 @@ require_relative 'git'
 require_relative 'slack'
 
 $slackErrorColor = "#e03131"
-$slackBuildColor = "#BADA55"
+$slackBuildColor = "#36a64f"
 
 def formatCodeString(code)
   return '```' + code + '```'
@@ -15,6 +15,38 @@ def getProjectChannelName()
 		return ENV['PROJECT_SLACK_CHANNEL'].to_s
 	else
 		return '#bitrise'
+	end
+end
+
+def getBitriseBuildURL()
+	if ENV['BITRISE_BUILD_URL'] != nil && !ENV['BITRISE_BUILD_URL'].to_s.empty?
+		return ENV['BITRISE_BUILD_URL'].to_s
+	else
+		return 'https://www.bitrise.io/dashboard'
+	end
+end
+
+def getBitriseTag()
+	if ENV['BITRISE_GIT_TAG'] != nil && !ENV['BITRISE_GIT_TAG'].to_s.empty?
+		return ENV['BITRISE_GIT_TAG'].to_s
+	else
+		return '(No tag found)'
+	end
+end
+
+def getBitriseTimestamp()
+	if ENV['BITRISE_BUILD_TRIGGER_TIMESTAMP'] != nil && !ENV['BITRISE_BUILD_TRIGGER_TIMESTAMP'].to_s.empty?
+		return ENV['BITRISE_BUILD_TRIGGER_TIMESTAMP'].to_s
+	else
+		return '(No time found)'
+	end
+end
+
+def getBitriseBranch()
+	if ENV['BITRISE_GIT_BRANCH'] != nil && !ENV['BITRISE_GIT_BRANCH'].to_s.empty?
+		return ENV['BITRISE_GIT_BRANCH'].to_s
+	else
+		return '(unknown branch)'
 	end
 end
 
@@ -52,15 +84,62 @@ def postBuildsSlack(builds)
     text = formatCodeString(comment)
   end
 
+  attachments = []
+  # Bitrise attachment
+  attachments.push({
+        {
+            "fallback": "Tag *#{getBitriseTag()}* triggered on *#{getBitriseBranch()}*, started *#{getBitriseTimestamp()}* by #{getCommitterName} (#{getCommitterMail}).",
+            "title": "Bitrise status",
+			      "text" : "Tag *#{getBitriseTag()}* triggered on *#{getBitriseBranch()}*, started *#{getBitriseTimestamp()}* by #{getCommitterName} (#{getCommitterMail}).",
+            "mrkdwn_in" : ["footer", "text"],
+            "actions": [
+              {
+                "type": "button",
+                "text": "Build log",
+                "url": getBitriseBuildURL(),
+                "style": "primary"
+              }
+            ]
+        }
+      })
+
   builds.each do |build|
     if build['error']
       parts = build['build'].split("/")
       apk = parts[-1]
-      text += "\nApk #{apk} (Hockey id: #{build['hockeyId']}) could not be deployed due to errors"
+      attachments.push({
+        {
+            "fallback": "Apk #{apk} (Hockey id: #{build['hockeyId']}) could not be deployed due to errors",
+            "color": "#F50057"
+            "title": "Apk #{apk} (Hockey id: #{build['hockeyId']}) could not be deployed due to errors",
+			      "actions": [
+              {
+                "type": "button",
+                "text": "Hockey page",
+                "url": "https://rink.hockeyapp.net/manage/apps/#{build['hockeyId']}",
+                "style": "danger"
+              }
+            ]
+        }
+      })
       next
     end
     if (build['latestHockeyVersion'] && !build['error'])
-      text += "\n<" + build['latestHockeyVersion']['download_url'] + "|" + build['latestHockeyVersion']['title'] + " v#{build['latestHockeyVersion']['shortversion']} (#{build['latestHockeyVersion']['version']})" + ">"
+      attachments.push({
+        {
+            "fallback": "#{build['latestHockeyVersion']['download_url']} #{build['latestHockeyVersion']['title']} v#{build['latestHockeyVersion']['shortversion']} (#{build['latestHockeyVersion']['version']})",
+            "color": $slackBuildColor
+            "title": "#{build['latestHockeyVersion']['download_url']} #{build['latestHockeyVersion']['title']} v#{build['latestHockeyVersion']['shortversion']} (#{build['latestHockeyVersion']['version']})",
+			      "actions": [
+              {
+                "type": "button",
+                "text": "Install",
+                "url": "https://rink.hockeyapp.net/manage/apps/#{build['hockeyId']}",
+                "style": "primary"
+              }
+            ]
+        }
+      })
     end
   end
 
@@ -72,16 +151,7 @@ def postBuildsSlack(builds)
       "channel" => getProjectChannelName(),
       "username" => 'bitrise-ci',
       "mrkdwn" => true,
-      "attachments" => [{
-                            "fallback" => fallback,
-                            "title" => title,
-                            "title_link" => getGithubPageUrl(),
-                            "text" => text,
-                            "author_name" => "#{getCommitterName} (#{getCommitterMail})",
-                            "color" => $slackBuildColor,
-                            "footer" => "Bitrise CI " + $version + ", report bugs and feature requests on the <https://trello.com/b/7Blqe5gH/bitrise-ci|Trello board>",
-                            "mrkdwn_in" => ["text"]
-                        }]
+      "attachments" => attachments
   }
   #puts "data = #{data}"
   result = runCurlJson(data, $slackUrl)
