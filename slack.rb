49823e67a00d9a9b10e7ee9b06d2aa68989084d1
Tiago Araujo <tiar@nodesagency.com>
2019-07-02 13:26:46 +0200

    removed datestamp

--- slack.rb
+++ slack.rb
@@ -144,10 +144,10 @@ def postBuildsSlack(builds)
   # Bitrise attachment
   attachments.push(
       {
-          :fallback => "Tag *#{getBitriseTag}* triggered on *#{getBitriseBranch}*, started *#{DateTime.strptime(getBitriseTimestamp,'%s')}* by #{getCommitterName} (#{getCommitterMail})",
+          :fallback => "Tag *#{getBitriseTag}* triggered on *#{getBitriseBranch}* by #{getCommitterName} (#{getCommitterMail})",
           :title => "Bitrise status",
           :color => message_color,
-          :text => "Tag *#{getBitriseTag}* triggered on *#{getBitriseBranch}*, started *#{DateTime.strptime(getBitriseTimestamp,'%s')}* by #{getCommitterName} (#{getCommitterMail})",
+          :text => "Tag *#{getBitriseTag}* triggered on *#{getBitriseBranch}* by #{getCommitterName} (#{getCommitterMail})",
           :mrkdwn_in => %w(footer text),
           :actions => [{
                            :type => "button",
